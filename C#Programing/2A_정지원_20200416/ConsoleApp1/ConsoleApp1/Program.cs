a9d38975c7261f75e8df83e8d004dcb6bc87927a
JeongJiWon <newmoonhana@naver.com>
2020-04-16 09:56:44 +0900

    GetType 사용 과제

---
+++ C#Programing/2A_정지원_20200416/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언.
+            Program p = new Program();
+            int a = 10;
+            float b = 0.1f;
+            string c = "abcd";
+
+            Console.WriteLine("\n");
+            //GetType() 사용.
+            p.Get_Type(a);
+            p.Get_Type(b);
+            p.Get_Type(c);
+        }
+
+        void Get_Type(Object val)   //GetType 사용 함수.
+        {
+            Console.WriteLine("{0}", val.GetType());
+        }
+    }
+}
